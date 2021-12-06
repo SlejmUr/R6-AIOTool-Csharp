61101af36b69865bf6bc80a4a56367b8c8c49807
SlejmUr <52343819+SlejmUr@users.noreply.github.com>
2021-12-06 15:05:15 +0100

    Updater
    simple script to update

---
+++ AIO-Updater.bat
@@ -0,0 +1,14 @@
+@echo off
+setlocal enableextensions enabledelayedexpansion
+
+if exist "Data\Update\AIO-Tools.exe" (
+mkdir Data\Old >nul
+move AIO-Tools.exe Data\Old >nul
+move Data\Update\AIO-Tools.exe . >nul
+rd /s /q Data\Update
+rd /s /q Data\Old
+echo Update Successful^!
+) else (
+echo Update file not found, probably you running the newest version^!
+)
+pause
